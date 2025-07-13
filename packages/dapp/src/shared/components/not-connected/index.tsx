682f4ba450b181a032fbad004f9e32f1e3fc1df8
igdev <dorultanianos@gmail.com>
2025-07-13 15:53:37 +0300

    feat: enhance NotConnected component with customizable title and description props

--- packages/dapp/src/shared/components/not-connected/index.tsx
+++ packages/dapp/src/shared/components/not-connected/index.tsx
@@ -1,11 +1,20 @@
-export default function NotConnected() {
+export interface Props {
+  title?: string
+  description?: string
+}
+
+const defaultProps = {
+  title: "🤖Looks like your wallet is not connected",
+  description: "Please connect your wallet so you can use this feature.",
+}
+export default function NotConnected(props?: Props) {
   return (
     <div className="text-center">
       <h1 className="text-white text-3xl">
-        🤖Looks like your wallet is not connected
+        {props?.title ?? defaultProps.title}
       </h1>
       <p className="text-white">
-        Please connect your wallet so you can use this feature.
+        {props?.description ?? defaultProps.description}
       </p>
     </div>
   )
