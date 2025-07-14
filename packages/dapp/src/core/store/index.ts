0f154906bb5b26be061df4066082a5b652a9a945
igdev <dorultanianos@gmail.com>
2025-07-14 09:58:57 +0300

    feat: add theme slice with persistence support using redux-persist

--- packages/dapp/src/core/store/index.ts
+++ packages/dapp/src/core/store/index.ts
@@ -1,15 +1,17 @@
-import {configureStore} from '@reduxjs/toolkit';
-import appReducer from '../../app-slice.ts';
+import { configureStore } from "@reduxjs/toolkit"
+import { persistStore } from "redux-persist"
+import appReducer from "../../app-slice.ts"
 
 const store = configureStore({
   reducer: {
-    app: appReducer
+    app: appReducer,
   },
-});
+})
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
-export default store;
+export const persistor = persistStore(store)
+export default store
