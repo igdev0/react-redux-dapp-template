a036104ea37e84801d9b503be0ccfba5c2209ba2
igdev <dorultanianos@gmail.com>
2025-07-14 11:10:42 +0300

    feat: update Counter component to use Button component and adjust color variables

--- packages/dapp/src/features/counter/components/counter.tsx
+++ packages/dapp/src/features/counter/components/counter.tsx
@@ -6,6 +6,7 @@ import {
 } from "@shared/hooks/generated.ts"
 import withConnected from "@shared/hocs/with-connected.tsx"
 import NotConnected from "@shared/components/not-connected"
+import { Button } from "@shared/ui/button.tsx"
 
 function Counter() {
   const chainId = useChainId()
@@ -23,7 +24,7 @@ function Counter() {
   }
   return (
     <div className="flex flex-col w-full items-center justify-center gap-5 mt-40">
-      <h1 className="text-4xl font-bold text-white">
+      <h1 className="text-4xl font-bold text-primary">
         Count{" "}
         <span className="text-gray-100">
           {count.data}
@@ -31,20 +32,12 @@ function Counter() {
         </span>
       </h1>
       <div className="flex gap-2">
-        <button
-          className="rounded-sm px-4 py-2 bg-amber-500 text-white disabled:bg-amber-400 cursor-pointer"
-          onClick={handleIncrement}
-          disabled={increment.isPending}
-        >
+        <Button onClick={handleIncrement} disabled={increment.isPending}>
           Increment
-        </button>
-        <button
-          className="rounded-sm px-4 py-2 bg-teal-500 text-white disabled:bg-teal-400 cursor-pointer"
-          onClick={handleDecrement}
-          disabled={decrement.isPending}
-        >
+        </Button>
+        <Button onClick={handleDecrement} disabled={decrement.isPending}>
           Decrement
-        </button>
+        </Button>
       </div>
     </div>
   )
