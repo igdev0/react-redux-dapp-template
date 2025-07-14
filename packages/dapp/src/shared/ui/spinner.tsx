e1c172c414496a445535c8d521bc151f260f3337
igdev <dorultanianos@gmail.com>
2025-07-14 15:46:08 +0300

    feat: add Spinner component for loading indication

---
+++ packages/dapp/src/shared/ui/spinner.tsx
@@ -0,0 +1,26 @@
+export interface Props {
+  size?: "sm" | "md" | "lg"
+}
+
+export default function Spinner(props: Props = { size: "md" }) {
+  let size: number
+  switch (props.size) {
+    case "sm":
+      size = 4
+      break
+    case "md":
+      size = 6
+      break
+    case "lg":
+      size = 10
+      break
+    default:
+      size = 6
+  }
+
+  return (
+    <div
+      className={`w-${size} aspect-square rounded-full border-2 border-transparent border-t-primary border-l-primary animate-spin`}
+    />
+  )
+}
