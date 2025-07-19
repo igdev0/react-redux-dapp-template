080e98b10308a2095ed33e8ec9c96c84798e28da
igdev <dorultanianos@gmail.com>
2025-07-19 15:11:05 +0300

    fix: notification service spec

--- packages/backend/src/notification/notification.service.spec.ts
+++ packages/backend/src/notification/notification.service.spec.ts
@@ -1,16 +1,22 @@
 import { NotificationService } from './notification.service';
-import { TestBed } from '@suites/unit';
+import { Mocked, TestBed } from '@suites/unit';
+import { Repository } from 'typeorm';
+import NotificationEntity from './entities/notification.entity';
 
 describe('NotificationService', () => {
   let service: NotificationService;
+  let notificationRepository: Mocked<Repository<NotificationEntity>>;
 
   beforeEach(async () => {
-    const { unit } = await TestBed.solitary(NotificationService).compile();
+    const { unit, unitRef } =
+      await TestBed.solitary(NotificationService).compile();
 
     service = unit;
+    notificationRepository = unitRef.get('NotificationEntityRepository');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(notificationRepository).toBeDefined();
   });
 });
