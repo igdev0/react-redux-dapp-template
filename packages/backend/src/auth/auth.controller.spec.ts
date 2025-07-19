4876f8b81a86f87c388f6575c446fa6d068729be
igdev <dorultanianos@gmail.com>
2025-07-19 14:59:25 +0300

    refactor: remove unnecessary message param

--- packages/backend/src/auth/auth.controller.spec.ts
+++ packages/backend/src/auth/auth.controller.spec.ts
@@ -2,20 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { getMockRes } from '@jest-mock/express';
 import { ModuleMocker } from 'jest-mock';
-import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import authConfig from './auth.config';
+import { HttpStatus } from '@nestjs/common';
 
-const moduleMocker = new ModuleMocker(global);
+const userModule = new ModuleMocker(global);
+const typeormModule = new ModuleMocker(global);
+const jwtModule = new ModuleMocker(global);
+
+jest.mock('siwe', () => ({
+  generateNonce: jest.fn(() => 'mocked-nonce'),
+  SiweMessage: jest.fn().mockImplementation(() => ({
+    nonce: 'mocked-nonce',
+    address: '0xMockedAddress',
+    verify: jest.fn().mockResolvedValue({ success: true }), // or customize this
+  })),
+}));
+
 describe('AuthController', () => {
   let controller: AuthController;
 
   const { res, clearMockRes } = getMockRes();
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forFeature(authConfig), CacheModule.register()],
       controllers: [AuthController],
     })
       .useMocker((token) => {
-        if (token === AuthService) {
-          return moduleMocker;
+        if (token === UserModule) {
+          return userModule;
+        }
+        if (token === TypeOrmModule) {
+          return typeormModule;
+        }
+        if (token === JwtModule) {
+          return jwtModule;
         }
         return new ModuleMocker(global);
       })
@@ -24,9 +49,14 @@ describe('AuthController', () => {
     controller = module.get<AuthController>(AuthController);
   });
 
-  it('should be defined', async () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should generate a new nonce', async () => {
     await controller.getNonce(res);
-    expect(res).toHaveBeenCalled();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+    expect(res.send).toHaveBeenCalledWith('mocked-nonce');
   });
 });
