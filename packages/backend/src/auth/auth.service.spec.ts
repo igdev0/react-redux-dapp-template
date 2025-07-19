d91c97db3c63187a9c5d752074cc03bbf4287603
igdev <dorultanianos@gmail.com>
2025-07-19 09:38:49 +0300

    test: auth sign in service

--- packages/backend/src/auth/auth.service.spec.ts
+++ packages/backend/src/auth/auth.service.spec.ts
@@ -16,11 +16,13 @@ const MOCKED_CONFIG = {
   secure: true,
 };
 
+const DATE_NOW = 1752905802352;
+
 describe('AuthService', () => {
   let service: AuthService;
   let jwtService: Mocked<JwtService>;
   let configService: Mocked<ConfigService>;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   let cacheManager: Mocked<CacheManagerStore>;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let userRepository: Mocked<Repository<User>>;
@@ -33,6 +35,7 @@ describe('AuthService', () => {
     jwtService = unitRef.get(JwtService);
     cacheManager = unitRef.get(CACHE_MANAGER);
     userRepository = unitRef.get('UserRepository');
+
     configService.get.mockImplementation((property: string) => {
       return MOCKED_CONFIG[property.split('.')[1]];
     });
@@ -58,6 +61,15 @@ describe('AuthService', () => {
 
   describe('Utils', () => {
     beforeEach(() => {
+      globalThis.crypto = {
+        subtle: crypto.subtle,
+        getRandomValues: jest.fn(),
+        randomUUID(): `${string}-${string}-${string}-${string}-${string}` {
+          return 'sample-uuid-created-just-now';
+        },
+      };
+
+      globalThis.Date.now = jest.fn().mockReturnValue(DATE_NOW);
       jwtService.sign.mockClear();
     });
 
@@ -98,6 +110,53 @@ describe('AuthService', () => {
       expect(refreshToken).toEqual('signed_refresh_token');
       expect(jwtService.sign).toHaveBeenCalledWith(payload, {
         expiresIn: MOCKED_CONFIG.refreshTokenTTL,
+      });
+    });
+  });
+
+  describe('Auth', () => {
+    it('should be able to sign in', async () => {
+      const user = {
+        id: 'some-user-uuid',
+        wallet_address: '0x00000',
+        created_at: new Date('20-05-1995'),
+        updated_at: new Date('20-05-1995'),
+        notifications: [],
+      };
+      const result = await service.signIn(user);
+
+      const accessTokenPayload = {
+        wallet_address: user.wallet_address,
+        sub: user.id,
+        jti: crypto.randomUUID(),
+      };
+      expect(jwtService.sign).toHaveBeenCalledWith(accessTokenPayload, {
+        expiresIn: MOCKED_CONFIG.accessTokenTTL,
+        secret: MOCKED_CONFIG.secret,
+      });
+
+      const refreshTokenPayload = {
+        sub: user.id,
+        jti: crypto.randomUUID(),
+        iat: Math.floor(Date.now() / 1000),
+      };
+
+      expect(jwtService.sign).toHaveBeenCalledWith(refreshTokenPayload, {
+        expiresIn: MOCKED_CONFIG.refreshTokenTTL,
+      });
+      expect(cacheManager.set).toHaveBeenCalledWith(
+        `refresh_token:${refreshTokenPayload.jti}`,
+        accessTokenPayload.sub,
+        refreshTokenPayload.iat,
+      );
+
+      expect(jwtService.sign).toHaveBeenCalledTimes(2);
+
+      expect(result).toEqual({
+        accessToken: service.generateAccessToken(accessTokenPayload),
+        refreshToken: service.generateRefreshToken(refreshTokenPayload),
+        accessTokenTTL: configService.get<number>('auth.accessTokenTTL'),
+        refreshTokenTTL: configService.get<number>('auth.refreshTokenTTL'),
       });
     });
   });
