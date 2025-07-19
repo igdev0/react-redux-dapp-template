67105141310c886e6c55a4ff94757e5bf4ac09e1
igdev <dorultanianos@gmail.com>
2025-07-19 14:32:02 +0300

    test(notification): mock the controller modules

--- packages/backend/src/notification/notification.controller.spec.ts
+++ packages/backend/src/notification/notification.controller.spec.ts
@@ -1,13 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationController } from './notification.controller';
+import { ModuleMocker } from 'jest-mock';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
+const typeormModule = new ModuleMocker(global);
+const authModule = new ModuleMocker(global);
+const userModule = new ModuleMocker(global);
 describe('NotificationController', () => {
   let controller: NotificationController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationController],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === TypeOrmModule) {
+          return typeormModule;
+        }
+        if (token === AuthModule) {
+          return authModule;
+        }
+
+        if (token === UserModule) {
+          return userModule;
+        }
+        return new ModuleMocker(global);
+      })
+      .compile();
 
     controller = module.get<NotificationController>(NotificationController);
   });
