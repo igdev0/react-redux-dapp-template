1228b64d2aa5ef7660e8ecf5f15f07db7e21740b
igdev <dorultanianos@gmail.com>
2025-07-20 15:55:53 +0300

    test(notification): add test case for when query limit is larger than 15

--- packages/backend/src/notification/notification.controller.spec.ts
+++ packages/backend/src/notification/notification.controller.spec.ts
@@ -4,16 +4,39 @@ import { ModuleMocker } from 'jest-mock';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
+import { User } from '../user/entities/user.entity';
+import { NotificationService } from './notification.service';
+import NotificationEntity from './entities/notification.entity';
+
+const user: User = {
+  id: 'user-id',
+  notifications: [],
+  wallet_address: '0x000',
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+const notification: NotificationEntity = {
+  id: 'some-id',
+  metadata: {},
+  message: 'Some message',
+  updated_at: new Date(),
+  created_at: new Date(),
+  user: { id: 'some-id', notifications: [] },
+  title: 'Some title',
+  type: 'system',
+  is_read: false,
+};
 
 const typeormModule = new ModuleMocker(global);
 const authModule = new ModuleMocker(global);
 const userModule = new ModuleMocker(global);
 describe('NotificationController', () => {
   let controller: NotificationController;
-
+  let service: NotificationService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationController],
+      providers: [NotificationService],
     })
       .useMocker((token) => {
         if (token === TypeOrmModule) {
@@ -29,11 +52,27 @@ describe('NotificationController', () => {
         return new ModuleMocker(global);
       })
       .compile();
-
+    service = module.get(NotificationService);
     controller = module.get<NotificationController>(NotificationController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be able to get notifications', async () => {
+    const res = jest
+      .spyOn(service, 'getNotifications')
+      .mockReturnValue(Promise.resolve([notification]));
+    await controller.getNotifications(user);
+    expect(res).toHaveBeenCalledWith(user.id, 0, 10); // defaults
+  });
+
+  it('should not be able to get more than 15 results per query ', async () => {
+    const res = jest
+      .spyOn(service, 'getNotifications')
+      .mockReturnValue(Promise.resolve([notification]));
+    await controller.getNotifications(user, 10, 1000);
+    expect(res).toHaveBeenCalledWith(user.id, 10, 15);
   });
 });
